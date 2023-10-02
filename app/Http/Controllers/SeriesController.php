64c28ae6d83e7c9d5315a105cacd147e2396bf9e
Pedro <pedrosousacoelhoo@gmail.com>
2023-10-02 01:19:18 -0300

    last-update

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,23 +2,27 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller{
-
-
-    public function index(Request $request){
+    public function index(){
         //return $request->url();
         //return redirect ('https://youtube.com');
 
-        $series = [
-            'breaking-Bad',
-            'Icaly',
-            'Suits'
-        ];
+        $series = DB::select('SELECT nome FROM series');
 
         return view('series.index' ) -> with('series', $series);
     }
     public function create(){
         return view('series.create');
+    }
+
+    public function store(Request $request){
+            $nomeSerie = $request -> input ('nome');
+            if(DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])){
+                return redirect('/series');
+            } else {
+                return "Deu erro";
+            }
     }
 }
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,10 +1,11 @@
 <x-layout title='Nova Série'>
 
-    <form action="" method='post'>
+    <form action="/series/salvar" method='post'>
+        @csrf
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" id="nome" name='nome' class="form-control">
         </div>
-        <input type="button" value="Adicionar" class="btn btn-dark">
+        <input type="submit" value="Adicionar" class="btn btn-primary">
     </form>
 </x-layout>
