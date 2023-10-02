2c281ef7bfba004232f52e1c90e16aac2ed3882f
Pedro <pedrosousacoelhoo@gmail.com>
2023-10-01 23:27:40 -0300

    Last-update

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,9 +1,10 @@
 <x-layout title='Nova Série'>
 
     <form action="" method='post'>
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name='nome'>
-        <button type='submit'>Criar</button>
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome:</label>
+            <input type="text" id="nome" name='nome' class="form-control">
+        </div>
+        <input type="button" value="Adicionar" class="btn btn-dark">
     </form>
-
 </x-layout>
